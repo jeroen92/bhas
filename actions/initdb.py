b5777af4a19da81eeaab516609203c12413195b1
Bram ter Borch <bramterborch@gmail.com>
2016-01-19 17:22:01 +0100

    initdb.py changed

--- actions/initdb.py
+++ actions/initdb.py
@@ -1,18 +1,22 @@
 import settings
 import csv
 from classes.hijack import *
-from classes.target import *
+from classes.prefix import *
+from classes.origin import *
 
 def initDb():
     print 'Initializing database'
     print 'Creating tables\n'
     try:
-        settings.DATABASE.create_tables([Hijack,Target], safe=True)
+        settings.DATABASE.create_tables([Prefix,Origin,Hijack], safe=True)
     except peewee.OperationalError as e:
-        print 'An error occured while creating the database tables.\nError message: {0}\n'.format(e)
+        print 'An error occurred while creating the database tables.\nError message: {0}\n'.format(e)
+        return 1
     with open(settings.INPUTFILE, 'rb') as inputprefix:
         print inputprefix
         reader = csv.reader(inputprefix)
         for row in reader:
-            print row
+            subnet = row.split('/')[0]
+            mask = row.split('/')[1]
+            target = Prefix(subnet=subnet, mask=mask)
     return 0
