f528441f26966852297e0293d3dbbf37bc11fdbb
Jeroen Schutrup <jeroenschutrup@hotmail.nl>
2016-01-19 17:16:59 +0100

    modified erd

--- classes/hijack.py
+++ classes/hijack.py
@@ -1,14 +1,15 @@
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 from classes.basemodel import *
-from classes.target import *
+from classes.prefix import *
 import datetime, settings
 
 class Hijack(BaseModel):
     id = BigIntegerField(unique=True, primary_key=True)
-    target = ForeignKeyField(Target, on_delete="CASCADE")
-    prefix = CharField()
-    mask = BigIntegerField()
+    prefix = ForeignKeyField(Prefix, on_delete="CASCADE")
+    subnet = CharField()
+    mask = IntegerField()
+    originAs = IntegerField()
     originUpstreamAs = IntegerField()
     createdAt = DateTimeField(default=datetime.datetime.now)
     withdrawedAt = DateTimeField()
