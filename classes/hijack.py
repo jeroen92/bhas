eb145f82ed072d9744830ae986b6c282871c6bdf
Jeroen Schutrup <jeroenschutrup@hotmail.nl>
2016-01-19 16:45:39 +0100

    changes

--- classes/hijack.py
+++ classes/hijack.py
@@ -1,10 +1,12 @@
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 from classes.basemodel import *
+from classes.target import *
 import datetime, settings
 
 class Hijack(BaseModel):
     id = BigIntegerField(unique=True, primary_key=True)
+    target = ForeignKeyField(Target, on_delete="CASCADE")
     prefix = CharField()
     mask = BigIntegerField()
     originUpstreamAs = IntegerField()
