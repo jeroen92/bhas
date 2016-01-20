89f95f5443eb367627c15e637169a189b4d9aa38
Jeroen Schutrup <jeroenschutrup@hotmail.nl>
2016-01-20 18:04:09 +0100

    Remove file

--- geoloc.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import requests 
-import json
-
-r = requests.get("https://stat.ripe.net/data/geoloc/data.json?resource=1103")
-for location in json.loads(r.text)['data']['locations']:
-    print location['country']
