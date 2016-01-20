3eea7edf9d76b076b14997ed9911d17f3cd59bcb
Jeroen Schutrup <jeroenschutrup@hotmail.nl>
2016-01-20 13:54:50 +0100

    Parse stdin announcements to Event objects

--- actions/bootstrap.py
+++ actions/bootstrap.py
@@ -1,21 +1,39 @@
-import fileinput, json, sys, logging
+import fileinput, json, sys, logging, datetime
 from classes.hijack import *
+from classes.event import *
 from classes.origin import *
 from classes.prefix import *
 
 def bootstrap():
     while True:
         line = sys.stdin.readline()
+        if line == '':
+            continue
         logging.debug('Bootstrap\t Got new line from stdin, trying to parse JSON')
         try:
-            event = json.loads(line)
+            announcement = json.loads(line)
         except ValueError:
             logging.debug(e)
-        logging.debug('Bootstrap\t Load json into dict: {0}'.format(event))
+        logging.debug('Bootstrap\t Load json into dict: {0}'.format(announcement))
         try:
-            prefix = event['neighbor']['message']['update']['announce']['ipv4 unicast'][event['neighbor']['ip']].keys()[0]
-            logging.debug('Bootstrap\t Stripping prefix from update message: {0}'.format(str(prefix)))
-            prefix = Prefix.create(subnet=prefix.split('/')[0], mask=prefix.split('/')[1])
+            logging.debug('Bootstrap\t Stripping attributes from update message')
+            announcementType = announcement['type']
+            prefix = announcement['neighbor']['message'][announcementType]['announce']['ipv4 unicast'][announcement['neighbor']['ip']].keys()[0]
+            subnet = prefix.split('/')[0]
+            mask = prefix.split('/')[1]
+            asPath = announcement['neighbor']['message']['update']['attribute']['as-path']
+            neighbor = announcement['neighbor']['ip']
+            timestamp = datetime.datetime.fromtimestamp(announcement['time']).strftime('%Y-%m-%d %H:%M:%S')
+            event = Event(
+                subnet=subnet,
+                mask=mask,
+                asPath=asPath,
+                neighbor=neighbor,
+                med=None,
+                announcementType=announcementType,
+                timestamp=timestamp
+            )
+            logging.debug('Bootstrap\t Parsed Event from update message: {0}'.format(str(event.__dict__)))
         except KeyError as e:
             logging.debug(e)
         except IntegrityError as e:
---
+++ classes/event.py
@@ -0,0 +1,15 @@
+class Event():
+    def __init__(self, subnet, mask, asPath, neighbor, med, announcementType, timestamp):
+        self.subnet = subnet
+        self.mask = mask
+        self.asPath = asPath
+        self.neighbor = neighbor
+        self.med = med
+        self.announcementType = announcementType
+        self.timestamp = timestamp
+
+    def __hash__(self):
+        return (self.prefix, self.mask, self.asPath, self.neighbor, self.med, self.announcementType, self.timestamp).__hash__()
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
