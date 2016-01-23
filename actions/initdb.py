91f376ae0dde7d7df61f203c0750532237b234a7
Jeroen Schutrup <jeroenschutrup@hotmail.nl>
2016-01-23 16:31:56 +0100

    bhas is now production ready

--- actions/initdb.py
+++ actions/initdb.py
@@ -6,15 +6,26 @@ import ipwhois, requests
 from classes.hijack import *
 from classes.prefix import *
 from classes.origin import *
+from netaddr import IPNetwork
 
 def writeOrigin(origin_as, prefix):
     originAsCc='XYZ'
     asGeolocation = requests.get("https://stat.ripe.net/data/geoloc/data.json?resource={0}".format(origin_as))
+    prefixNetwork = IPNetwork(prefix.subnet + '/' + str(prefix.mask))
     for location in json.loads(asGeolocation.text)['data']['locations']:
-        # TODO: check if prefix is within supernet
-        if prefix.subnet+'/'+str(prefix.mask) in location['prefixes']:
-            originAsCc = location['country']
-            break
+        for locationPrefix in location['prefixes']:
+            if prefixNetwork in IPNetwork(locationPrefix):
+                originAsCc = location['country']
+                break
+    # If no location was found, try current prefix geolocation
+    if originAsCc == 'XYZ':
+        print "Could not get location data from origin AS, now trying prefix geolocation"
+        prefixGeolocation = requests.get("https://stat.ripe.net/data/geoloc/data.json?resource={0}".format(prefix.subnet + '/' + str(prefix.mask)))
+        for location in json.loads(prefixGeolocation.text)['data']['locations']:
+            for locationPrefix in location['prefixes']:
+                if prefixNetwork in IPNetwork(locationPrefix):
+                    originAsCc = location['country']
+                    break
     try:
         Origin.get(Origin.originAs == origin_as, Origin.prefix == prefix)
     except:
@@ -41,6 +52,7 @@ def initDb():
                 # string.ljust(x) appends x spaces to the right end of the string
                 print 'Error for prefix {0}{1}'.format(row[0].ljust(49),e)
                 pass
+
     for prefix in Prefix.select():
         prefixm = prefix.subnet+'/'+str(prefix.mask)
         url = "https://stat.ripe.net/data/whois/data.json?resource={0}".format(prefixm)
@@ -48,12 +60,13 @@ def initDb():
         stdout,stderr = system_out.communicate()
         dict = json.loads(stdout)
         found = False
-        for list in dict['data']['irr_records']:
-            for value in list:
-                if value['key'] == "origin":
-                    found = True
-                    origin_as = value['value']
-                    writeOrigin(origin_as, prefix)
+        if 'irr_records' in dict['data'].keys():
+            for list in dict['data']['irr_records']:
+                for value in list:
+                    if value['key'] == "origin":
+                        found = True
+                        origin_as = value['value']
+                        writeOrigin(origin_as, prefix)
         if not found:
             origin_as = ipwhois.IPWhois(prefix.subnet).lookup()['asn']
             writeOrigin(origin_as, prefix)
--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 from playhouse.sqlite_ext import SqliteExtDatabase
+from playhouse.sqlite_ext import MySQLDatabase
 
 global DATABASE, VERSION, APPNAME_SHORT, INPUTFILE
-DATABASE = SqliteExtDatabase('./db/bhas.db')
+#DATABASE = SqliteExtDatabase('./db/bhas.db')
+DATABASE = MySQLDatabase("bhas", host="localhost", port=3306, user="root", passwd="monitoring")
+
 VERSION = '0.1'
 APPNAME_SHORT = 'bhas'
 INPUTFILE = './input.csv'
